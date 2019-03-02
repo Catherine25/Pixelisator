145eb84b62153df73f73d79015f4a0670ef40db4
Catherine <Catherine25@users.noreply.github.com>
2019-03-02 23:14:06 +0200

    Some refactorings

--- Pixelisator/Data/Progress.cs
+++ Pixelisator/Data/Progress.cs
@@ -1,33 +1,24 @@
 ﻿using System.Drawing;
-using System.Windows.Forms;
 
 namespace Pixelisator.Data
 {
     static class Progress
     {
-        static Progress()
-        {
-        }
         //data
         static public int Counter { get; set; }
+        static public int Step { get; set; }
         static public Graphics OutGraphics { get; set; }
-        static Image WorkImage { get; set; }
-        
-        //MessageBox.Show("Completed! There are " + Progress.Counter.ToString() + " pixels changed!");
-        //static Progress() => Counter = 0;
+        static public Image WorkImage { get; set; }
 
         static public void ShowResult()
         {
-            OutGraphics = Graphics.FromImage(Patterns.Bitmap);
-            WorkImage = Patterns.Bitmap;
+           OutGraphics = Graphics.FromImage(WorkImage);
 
             OutGraphics.DrawImage(WorkImage,
                 new Rectangle(0, 0, WorkImage.Width, WorkImage.Height),
-                0, 0, WorkImage.Width, WorkImage.Height, GraphicsUnit.Pixel);
+                0, 0, WorkImage.Width, WorkImage.Height, GraphicsUnit.Pixel, Patterns.imageAttributes);
 
             OutGraphics.Dispose();
         }
-
-        //static void ShowMessage() => MessageBox.Show("Completed! There are " + Progress.Counter.ToString() + " pixels changed!");
     }
 }
