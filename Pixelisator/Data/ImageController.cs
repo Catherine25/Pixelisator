fa826463d50716cce1d49f329df95f21f67e9b79
Catherine <Catherine25@users.noreply.github.com>
2019-03-03 22:07:22 +0200

    Removed WorkImage; Added setImageToBitmap() and getBitmapAsImage();

--- Pixelisator/Data/ImageController.cs
+++ Pixelisator/Data/ImageController.cs
@@ -3,23 +3,18 @@ using System.Drawing;
 namespace Pixelisator.Data {
     static class ImageController {
         //accessors
-        public static Image WorkImage {
-            get { return workImage; }
-            set { workImage = value; }
-            }
-        public static Image LoadedImage {
-            get { return loadedImage; }
-            set { loadedImage = value; }
-            }
+        // public static Image WorkImage {
+        //     get { return workImage; }
+        //     set { workImage = value; }
+        //     }
 
         //data
-        private static Image workImage;
-        private static Image loadedImage;
+        //private static Image workImage;
         private static Bitmap bitmap;
 
         //metods
-        public static void ImageToBitmap() { bitmap = (Bitmap)Progress.WorkImage; }
-        public static void BitmapToImage() { Progress.WorkImage = bitmap; }
+        public static void setImageToBitmap(Image image) { bitmap = (Bitmap)image; }
+        public static Image getBitmapAsImage() { return (Image)bitmap; }
         public static Color getPixel(int x, int y) { return bitmap.GetPixel(x, y); }
         public static void setPixel(int x, int y, Color c) { bitmap.SetPixel(x, y, c); }
         public static int getWorkImageWidth() { return bitmap.Width; }
